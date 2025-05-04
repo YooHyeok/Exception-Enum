a53d9b8de11a50de9ed47100a3c1ebe710f6da77
YooHyeok <yjk7454@naver.com>
2025-05-05 02:43:53 +0900

    refactor: ExceptionService 클래스 리팩토링

--- src/main/java/com/example/exception/ExceptionService.java
+++ src/main/java/com/example/exception/ExceptionService.java
@@ -5,78 +5,79 @@ import org.springframework.stereotype.Service;
 @Service
 public class ExceptionService {
 
-    private StatusEnum catchStatus() {
-        String statusA = "statusA";
-        String statusB = "statusB";
-        String statusC = "statusC";
-
-        return switch (statusC) {
-            case "statusA" -> StatusEnum.STATUS_A;
-            case "statusB" -> StatusEnum.STATUS_B;
-            case "statusC" -> StatusEnum.STATUS_C;
-            default -> StatusEnum.PASS;
-        };
-    }
 
     public void exceptionA() {
-        StatusEnum statusEnum = StatusEnum.PASS;
+        ExcptionStatus statusEnum = ExcptionStatus.PASS;
         try {
 
-            statusEnum = catchStatus(); // statusEnum: PASS → STATUS_C 로 초기화됨
-            if (statusEnum != StatusEnum.PASS) {
+            /* 공통화 메소드 추출될 로직 가정 */
+            
+            statusEnum = ExcptionStatus.catchStatus(ExcptionStatus.STATUS_A); // statusEnum: PASS → STATUS_C 로 초기화됨
+            if (statusEnum != ExcptionStatus.PASS) {
                 throw new CustomException();
             }
 
-        } catch (CustomException cae) {
+        } catch (CustomException ce) {
             throw new CustomException(statusEnum);
+        } catch (ServerException se) {
+            throw new ServerException(statusEnum);
         } catch (Exception e) {
 
         }
     }
 
     /**
-     * [이슈 케이스] 주소값을 공유하지 못하는 경우
+     * [이슈 발생] 주소값을 공유하지 못하는 경우
      * 코드 공통화를 위한 특정 로직 commonRefact() 메소드 추출 리팩토링
      * StatusEnum을 commonRefact() 메소드 안에서 값을 PASS → STATUS_C 변경.
-     * 상수 특성을 지닌 Enum 변수 주소값을 통한 초기화 불가.
+     * 상수 특성을 지닌 Enum 변수 주소값을 통한 초기화 불가. (메소드 내 지역변수로 취급됨)
      * 예상: STATUS_C
      * 결과: PASS
      */
     public void exceptionB() {
-        StatusEnum statusEnum = StatusEnum.PASS;
+        ExcptionStatus statusEnum = ExcptionStatus.PASS;
         try {
-            commonRefactA(statusEnum); // StatusEnum을 메소드 안에서 값을 변경시킴. PASS → STATUS_C 로 초기화됨
-            throw new CustomException();
-
-        } catch (CustomException cae) {
+            commonRefactA(statusEnum);
+        } catch (CustomException ce) {
             throw new CustomException(statusEnum); // 초기값을 변경할수 없으므로 초기 PASS로 찍힘
+        } catch (ServerException se) {
+            throw new ServerException(statusEnum);
         } catch (Exception e) {
 
         }
     }
 
     /**
-     *[이슈 케이스]  주소값을 공유하지 못하는 경우
+     *[이슈 해결] 주소값을 공유하지 못하는 경우
      * throw 와 동시에 Exception 클래스의 생성자 매개면수로 초기화 해 준 뒤 getter로 꺼내어 다시 던진다
      */
     public void exceptionC() {
-        StatusEnum statusEnum = StatusEnum.PASS;
         try {
-            commonRefactB(statusEnum);
-
-        } catch (CustomException cae) {
-            throw new CustomException(cae.getStatusEnum());
+            commonRefactB();
+        } catch (CustomException ce) {
+            throw new CustomException(ce.getStatusEnum());
+        } catch (ServerException se) {
+            throw new ServerException(se.getStatusEnum());
         } catch (Exception e) {
 
         }
     }
-    private void commonRefactA(StatusEnum statusEnum) {
-        statusEnum = catchStatus();
+    private void commonRefactA(ExcptionStatus statusEnum) {
+
+        /* 공통화 메소드 추출된 로직 가정 */
+        
+        statusEnum = ExcptionStatus.catchStatus(ExcptionStatus.STATUS_B); // PASS → STATUS_B로 변경 (지역변수로 취급됨.)
+        if (statusEnum != ExcptionStatus.PASS) {
+            throw new CustomException();
+        }
     }
 
-    private void commonRefactB(StatusEnum statusEnum) {
-        statusEnum = catchStatus();
-        if (statusEnum != StatusEnum.PASS) {
+    private void commonRefactB() {
+
+        /* 공통화 메소드 추출된 로직 가정 */
+        
+        ExcptionStatus statusEnum = ExcptionStatus.catchStatus(ExcptionStatus.STATUS_C);
+        if (statusEnum != ExcptionStatus.PASS) {
             throw new CustomException(statusEnum);
         }
     }
